6c12b0f79b9b30a4e4a03de128d0c550a72579f8
Maxime Mouchet <max@maxmouchet.com>
2018-11-25 15:39:12 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atlas_df",
-    version="0.0.2",
+    version="0.0.4",
     author="Maxime Mouchet",
     author_email="max@maxmouchet.com",
     description="A dataframe-oriented interface to RIPE Atlas.",
